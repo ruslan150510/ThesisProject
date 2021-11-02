cf06982df9275408ffbc9e543ebb1126cb2bffa0
ruslan150510 <rakbashev25@gmail.com>
2021-11-02 15:47:07 +0500

    diplom_izm5_dop3_9

--- src/main/java/main/config/MvcConfig.java
+++ src/main/java/main/config/MvcConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**")
-                .addResourceLocations("classpath:/upload/");
+        registry.addResourceHandler("\\upload\\**")
+                .addResourceLocations("classpath:\\upload\\");
     }
 }
--- src/main/java/main/config/SecurityConfig.java
+++ src/main/java/main/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/**", "/upload/**", "/resources/upload/**").permitAll()
+                .antMatchers("/**", "\\upload\\**", "\\resources\\upload\\**").permitAll()
                 .anyRequest().authenticated();
     }
 
